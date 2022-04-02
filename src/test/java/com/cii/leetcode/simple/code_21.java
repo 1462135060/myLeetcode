4d3830bb072d5cbde87e0f89792a718a911eaff5
Cjt <1462135060@qq.com>
2022-04-02 15:00:55 +0800

    单链表操作

--- src/test/java/com/cii/leetcode/simple/code_21.java
+++ src/test/java/com/cii/leetcode/simple/code_21.java
@@ -44,6 +44,29 @@ public class code_21 {
         return res.next;
     }
 
+    public ListNode mergeTwoLists1(ListNode list1, ListNode list2) {
+        ListNode res = new ListNode(-1);
+        ListNode cur = res;
+        ListNode p1 = list1;
+        ListNode p2 = list2;
+        while (p1 != null && p2 != null) {
+            if (p1.val < p2.val) {
+                cur.next = p1;
+                p1 = p1.next;
+            } else {
+                cur.next = p2;
+                p2 = p2.next;
+            }
+            cur = cur.next;
+        }
+        if (p1 == null) {
+            cur.next = p2;
+        } else {
+            cur.next = p1;
+        }
+        return res.next;
+    }
+
 
     /**
      * 示例 1：
@@ -60,9 +83,9 @@ public class code_21 {
      */
     @Test
     void test() {
-        printListNode(mergeTwoLists(getListNode(new int[]{1,2,4}), getListNode(new int[]{1,3,4})));
-        printListNode(mergeTwoLists(getListNode(new int[]{}), getListNode(new int[]{})));
-        printListNode(mergeTwoLists(getListNode(new int[]{}), getListNode(new int[]{0})));
+        printListNode(mergeTwoLists1(getListNode(new int[]{1,2,4}), getListNode(new int[]{1,3,4})));
+        printListNode(mergeTwoLists1(getListNode(new int[]{}), getListNode(new int[]{})));
+        printListNode(mergeTwoLists1(getListNode(new int[]{}), getListNode(new int[]{0})));
     }
 
     private ListNode getListNode(int [] ints) {
